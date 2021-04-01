7bce769c58dd3d9766fe348d771bccb3475500b1
Phan Khoi <820929@stma.k12.mn.us>
2021-04-01 10:27:18 -0500

    c

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+def equalTo(a, b):
+    if a == b:
+        return True
+    else:
+        return False
+
+def lessOrEqual(m, l):
+    if m <= l:
+        return True
+    else:
+        return False
+
+def lessOrEqual(c, k):
+    if c == k:
+        return True
+    else:
+        return False
+
+def greaterOrEqual(s, d):
+    if s => d:
+        return True
+    else:
+        return False
+
+print ()
+
